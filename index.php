e5b4659cbd9edcac7738d817bab419cfefe0f4ab
Adam Tester <AdamTester@users.noreply.github.com>
2015-09-14 10:21:10 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$redirect = getenv('REDIRECT');
+if($redirect) {
+    header('Location: ' . $redirect);
+} else {
+    header('HTTP/1.0 404 Not Found');
+    echo "<h1>404 Not Found</h1>";
+    echo "The page that you have requested could not be found.";
+    exit();
+}
